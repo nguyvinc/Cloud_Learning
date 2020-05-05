a4060176b4ea4ec7b0f1bb65f7eefc719771d602
nguyvinc <29743521+nguyvinc@users.noreply.github.com>
2020-05-04 18:45:59 -0700

    Updated mysqlPool to use mongo connection

---
+++ Assignment2 - Mongo/lib/mongo.js
@@ -0,0 +1,25 @@
+const {MongoClient} = require("mongodb");
+
+const mongoHost = process.env.MONGO_HOST || 'localhost';
+const mongoPort = process.env.MONGO_PORT || '27017';
+const mongoDB = process.env.MONGO_DATABASE;
+const mongoUser = process.env.MONGO_USER;
+const mongoPassword = process.env.MONGO_PASSWORD;
+
+const mongoUrl = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDB}`;
+
+let db = null;
+
+exports.connectToDB = function(callback){
+    MongoClient.connect(mongoUrl, function(err, client){
+        if(err){
+            throw err;
+        }
+        db = client.db(mongoDB);
+        callback();
+    });
+};
+
+exports.getDBReference = function (){
+    return db;
+};
