000f507b4eaaf5093dda7f9055daa620d93e4c43
nguyvinc <29743521+nguyvinc@users.noreply.github.com>
2020-05-29 22:10:32 -0700

    Rearranged code

--- Assignment4/resizer.js
+++ Assignment4/resizer.js
@@ -6,10 +6,10 @@ const {connectToDB, getDBReference} = require("./lib/mongo");
 const {connectToRabbit, getChannel} = require("./lib/rabbit");
 const {getImageDownloadStreamById, updateOriginalPhotoById} = require("./models/photo");
 
-function resizeAndUpload(newPhotoSizes, imageChunks, id, bucket, size){
+function resizeAndUpload(newPhotoSizes, image, id, bucket, size){
     console.log("Creating ", size, "p image");
     //Resize the image into a buffer
-    const buffer = sharp(imageChunks).resize(size, size).toFormat("jpeg").toBuffer();
+    const buffer = sharp(image).resize(size, size).toFormat("jpeg").toBuffer();
 
     //Open an upload stream to the GridFS bucket
     const uploadStream = bucket.openUploadStream(id.concat('-', size));
@@ -28,43 +28,34 @@ function resizeAndUpload(newPhotoSizes, imageChunks, id, bucket, size){
 
 //Connect to MongoDB
 connectToDB(async() => {
-    //Get RabbitMQ message channel
-    let channel = getChannel();
-
     //Connect to RabbitMQ
-    while(channel == null){
-        await connectToRabbit("photos");
-        setTimeout(function(){channel = getChannel();}, 1000);
-    }
+    await connectToRabbit("photos");
     console.log("Resizer connected to Rabbit");
+
+    //Get RabbitMQ message channel
+    const channel = getChannel();
 
     //Consume upload message
     channel.consume("photos", msg => {
-        console.log(msg);
-        console.log("Message grabbed");
-
         //Grab id of uploaded photo
         const id = msg.content.toString();
         console.log("Image id grabbed: ", id);
-        let imageChunks = [];
+        const imageChunks = [];
 
         //Get the download stream of the image by id
         getImageDownloadStreamById(id)
-            .on("file", (file) => {
-                console.log("Image retrieved");
-            })
-            .on("data", chunk => {
+            .on("data", (chunk) => {
                 //Push each chunk to the array
-                console.log("Downloading image chunks");
                 imageChunks.push(chunk);
             })
             .on("end", async() => {
                 console.log("Image downloaded");
+                const image = Buffer.concat(imageChunks);
 
                 let newPhotoSizes = {orig: id};
                 //Grab the image's width and height
-                const width = sharp(imageChunks).width;
-                const height = sharp(imageChunks).height;
+                const width = sharp(image).width;
+                const height = sharp(image).height;
                 console.log("Image dimensions: ", width, " x ", height);
 
                 //Get the GridFS bucket to interact with the database
@@ -73,19 +64,22 @@ connectToDB(async() => {
 
                 //Check image's width and height
                 if(width >= 1024 && height >= 1024){
-                    resizeAndUpload(newPhotoSizes, imageChunks, id, bucket, 1024);
+                    resizeAndUpload(newPhotoSizes, image, id, bucket, 1024);
                 }
                 if(width >= 640 && height >= 640){
-                    resizeAndUpload(newPhotoSizes, imageChunks, id, bucket, 640);
+                    resizeAndUpload(newPhotoSizes, image, id, bucket, 640);
                 }
                 if(width >= 256 && height >= 256){
-                    resizeAndUpload(newPhotoSizes, imageChunks, id, bucket, 256);
+                    resizeAndUpload(newPhotoSizes, image, id, bucket, 256);
                 }
                 if(width >= 128 && height >= 128){
-                    resizeAndUpload(newPhotoSizes, imageChunks, id, bucket, 128);
+                    resizeAndUpload(newPhotoSizes, image, id, bucket, 128);
                 }
                 //Update the original photo, storing the id's of all the resized images
                 const result = await updateOriginalPhotoById(id, photoSizes);
+            })
+            .on("error", (err) =>{
+                console.log(err);
             });
         channel.ack(msg);
     });
