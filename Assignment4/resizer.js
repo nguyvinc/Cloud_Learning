e9155bd33ebee272cb87f2643b7733390eee46fb
nguyvinc <29743521+nguyvinc@users.noreply.github.com>
2020-05-30 19:55:41 -0700

    Resizer consumer properly works

--- Assignment4/resizer.js
+++ Assignment4/resizer.js
@@ -1,36 +1,43 @@
-const sharp = require("sharp");
+const sizeOf = require("image-size");
+const jimp = require("jimp");
+const stream = require("stream");
 const {GridFSBucket} = require("mongodb");
-const fs = require("fs");
 
 const {connectToDB, getDBReference} = require("./lib/mongo");
 const {connectToRabbit, getChannel} = require("./lib/rabbit");
 const {getImageDownloadStreamById, updateOriginalPhotoById} = require("./models/photo");
 
-function resizeAndUpload(newPhotoSizes, image, id, bucket, size){
-    console.log("Creating ", size, "p image");
-    //Resize the image into a buffer
-    const buffer = sharp(image).resize(size, size).toFormat("jpeg").toBuffer();
+async function resizeAndUpload(image, id, bucket, size){
+    return new Promise(async(resolve) => {
+        //Resize the image into a buffer
+        const photo = await jimp.read(image);
+        await photo.resize(size, size);
+        const buffer = await photo.getBufferAsync("image/jpeg");
 
-    //Open an upload stream to the GridFS bucket
-    const uploadStream = bucket.openUploadStream(id.concat('-', size));
+        //Open an upload stream to the GridFS bucket
+        const uploadStream = bucket.openUploadStream(id.concat('-', size));
 
-    //Create a read stream for the buffer and pipe it to the GridFS upload stream
-    fs.createReadStream(buffer).pipe(uploadStream)
-        .on("error", (err) => {
-            console.error(err);
-        })
-        .on("finish", (result) => {
-            //When the upload is finished, save the id of the resized image to the object
-            newPhotoSizes[size] = result._id;
-            console.log(size, "p image successfully saved");
-        });
+        //From StackOverflow https://stackoverflow.com/questions/16038705/how-to-wrap-a-buffer-as-a-stream2-readable-stream
+        //Initiate the source
+        const bufferStream = new stream.PassThrough();
+        //Write buffer into stream
+        bufferStream.end(buffer);
+        //Pipe the stream to the GridFS upload stream
+        bufferStream.pipe(uploadStream)
+            .on("error", (err) => {
+                console.error(err);
+            })
+            .on("finish", (result) => {
+                //When the upload is finished, save the id of the resized image to the object
+                resolve(result._id);
+            });
+    });
 }
 
 //Connect to MongoDB
 connectToDB(async() => {
     //Connect to RabbitMQ
     await connectToRabbit("photos");
-    console.log("Resizer connected to Rabbit");
 
     //Get RabbitMQ message channel
     const channel = getChannel();
@@ -39,7 +46,6 @@ connectToDB(async() => {
     channel.consume("photos", msg => {
         //Grab id of uploaded photo
         const id = msg.content.toString();
-        console.log("Image id grabbed: ", id);
         const imageChunks = [];
 
         //Get the download stream of the image by id
@@ -49,37 +55,31 @@ connectToDB(async() => {
                 imageChunks.push(chunk);
             })
             .on("end", async() => {
-                console.log("Image downloaded");
                 const image = Buffer.concat(imageChunks);
 
                 let newPhotoSizes = {orig: id};
                 //Grab the image's width and height
-                const width = sharp(image).width;
-                const height = sharp(image).height;
-                console.log("Image dimensions: ", width, " x ", height);
+                const dimensions = sizeOf(image);
 
                 //Get the GridFS bucket to interact with the database
                 const db = getDBReference();
                 const bucket = new GridFSBucket(db, {bucketName: "photos"});
 
                 //Check image's width and height
-                if(width >= 1024 && height >= 1024){
-                    resizeAndUpload(newPhotoSizes, image, id, bucket, 1024);
+                if(dimensions.width >= 1024 && dimensions.height >= 1024){
+                    newPhotoSizes["1024"] = await resizeAndUpload(image, id, bucket, 1024);
                 }
-                if(width >= 640 && height >= 640){
-                    resizeAndUpload(newPhotoSizes, image, id, bucket, 640);
+                if(dimensions.width >= 640 && dimensions.height >= 640){
+                    newPhotoSizes["640"] = await resizeAndUpload(image, id, bucket, 640);
                 }
-                if(width >= 256 && height >= 256){
-                    resizeAndUpload(newPhotoSizes, image, id, bucket, 256);
+                if(dimensions.width >= 256 && dimensions.height >= 256){
+                    newPhotoSizes["256"] = await resizeAndUpload(image, id, bucket, 256);
                 }
-                if(width >= 128 && height >= 128){
-                    resizeAndUpload(newPhotoSizes, image, id, bucket, 128);
+                if(dimensions.width >= 128 && dimensions.height >= 128){
+                    newPhotoSizes["128"] = await resizeAndUpload(image, id, bucket, 128);
                 }
                 //Update the original photo, storing the id's of all the resized images
-                const result = await updateOriginalPhotoById(id, photoSizes);
-            })
-            .on("error", (err) =>{
-                console.log(err);
+                await updateOriginalPhotoById(id, newPhotoSizes);
             });
         channel.ack(msg);
     });
